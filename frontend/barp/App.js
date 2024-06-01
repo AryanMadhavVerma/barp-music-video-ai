a4645383cbab5adbd64ddb5515a7c8bdc574fb48
priyanshu-09 <priyanshushukla9801@gmail.com>
2024-06-01 21:29:29 +0530

    Loader and Video upload

--- frontend/barp/App.js
+++ frontend/barp/App.js
@@ -7,8 +7,13 @@ import {
   NotoSans_800ExtraBold,
   useFonts,
 } from "@expo-google-fonts/noto-sans";
+import { useState } from "react";
+import * as FileSystem from "expo-file-system";
+import * as Permissions from "expo-permissions";
+import Loader from "./src/Loader";
 
 export default function App() {
+  const [uploading, setUploading] = useState(false);
   let [fontsLoaded, fontError] = useFonts({
     NotoSans_800ExtraBold,
     NotoSans_500Medium,
@@ -19,7 +24,70 @@ export default function App() {
     return null;
   }
 
-  return <VideoUploadScreen />;
+  const readFile = async (fileUri) => {
+    const fileData = await FileSystem.readAsStringAsync(fileUri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    return fileData;
+  };
+
+  const uploadVideoAudio = async (videouri, audiouri) => {
+    if (!videouri || !audiouri) return;
+
+    setUploading(true);
+
+    const videoFileData = await readFile(videouri);
+    const audioFileData = await readFile(audiouri);
+
+    // const uriParts = video.split(".");
+    // const fileType = uriParts[uriParts.length - 1];
+
+    const formData = new FormData();
+    // formData.append("video_file", videoFileData);
+    // formData.append("audio_file", audioFileData);
+
+    formData.append("video_file", {
+      uri: videouri,
+      name: "video.mov",
+      type: "video/mov",
+      data: videoFileData,
+    });
+
+    formData.append("audio_file", {
+      uri: audiouri,
+      name: "audio.mp3",
+      type: "audio/mpeg",
+      data: audioFileData,
+    });
+
+    console.log("Sending the Result to the server...");
+    //console.log(formData);
+
+    try {
+      const response = await fetch(
+        "https://regular-adder-sadly.ngrok-free.app/predict",
+        {
+          method: "POST",
+          body: formData,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error("Error uploading video:", error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  if (uploading) {
+    return <Loader />;
+  }
+  return <VideoUploadScreen handleOnUpload={uploadVideoAudio} />;
 }
 
 const styles = StyleSheet.create({
