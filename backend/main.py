2a367bd128ab991983673b13fabc5240d82fd065
Rajath Aithal <rajathlikeslemons@gmail.com>
2024-06-01 22:29:12 +0530

    pulled from main

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,14 @@ from fastapi import FastAPI, UploadFile
 from gemini import get_prompt_for_suno
 from pydantic import Field
 from servers.suno import generate_suno_song
+import sys
+import os
+import urllib.request
 
 data_folder = "./data/temp"
 VIDEO_FILE_PATH = "./data/temp/video_file.mp4"
 AUDIO_FILE_PATH = "./data/temp/audio_file.mp3"
+SUNO_SONG_FILE_PATH = "./data/temp/suno_song.mp3"
 
 app = FastAPI()
 
@@ -27,6 +31,10 @@ if os.environ.get("NGROK_AUTHTOKEN", ""):
 def predict(video_file: Optional[UploadFile], audio_file: Optional[UploadFile] = File(None)):
     result = ""
     # download file to local
+    print("-------")
+    print(video_file)
+    print(audio_file)
+    print("-------")
     if video_file is not None:
         try:
             with open(VIDEO_FILE_PATH, 'wb') as f:
@@ -50,9 +58,21 @@ def predict(video_file: Optional[UploadFile], audio_file: Optional[UploadFile] =
     # run gemini model to get results
     result = get_prompt_for_suno(video_file_path=VIDEO_FILE_PATH, audio_file_path=AUDIO_FILE_PATH)
 
+
     # call suno to get results
     suno_song = generate_suno_song(rhyme=result['rhyme'], song_type=result['song_type'], title=result['title'])
-    
+    print(suno_song)
+    url_to_download = suno_song.get("audio_url", "")
+    url_to_download = "https://audiopipe.suno.ai/?item_id=f22d86ef-3d17-4fc4-9f58-f25e1621f6b3"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+    req = urllib.request.Request(url_to_download, headers=headers)
+
+    try:
+        with urllib.request.urlopen(req) as response, open(SUNO_SONG_FILE_PATH, 'wb') as out_file:
+            out_file.write(response.read())
+    except Exception as e:
+        print(f'HTTP Error: {e.code} {e.reason}')
     # remove voice from suno results
     ## clone voice
     cloned_voice = get_cloned_voice(file_path=AUDIO_FILE_PATH)
