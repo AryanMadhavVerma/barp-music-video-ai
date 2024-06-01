9d3400b4c6d38251ad02618921d5646ad48bd5b3
Aryan Madhav Verma <amv190301@gmail.comm>
2024-06-02 02:38:17 +0530

    change decibel levels of audio

--- backend/utils/voice_utils/generate_final_mix.py
+++ backend/utils/voice_utils/generate_final_mix.py
@@ -5,11 +5,12 @@ def generate_final_mix(background, overlay):
     overlay = AudioSegment.from_wav(overlay)
     background = AudioSegment.from_file(background)
 
+    amplified_overlay = overlay + 10  # Increase the volume of the overlay by 10 dB
     # Reduce the background volume by 1-2 dB; test and adjust for desired "80%" effect
     reduced_background = background - 4  # Adjust dB here to fit the "80%" subjective requirement
 
     # Mixing the tracks (overlay the vocal track onto the background)
-    final_mix = reduced_background.overlay(overlay, position=0)
+    final_mix = reduced_background.overlay(amplified_overlay, position=0)
 
     # Export the final mix to an MP3 file
     final_mix.export("./data/temp/final_song_mix.mp3", format="mp3")
