ac6253dae75e1975af634b3f9d197da54b6e5640
priyanshu-09 <priyanshushukla9801@gmail.com>
2024-06-02 06:38:39 +0530

    Loader changed

--- frontend/src/Loader.js
+++ frontend/src/Loader.js
@@ -1,23 +1,41 @@
-import React from "react";
+import LottieView from "lottie-react-native";
+import React, { useRef } from "react";
 import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { CustomHeadingText, CustomText } from "./components/CustomText";
 
-const Loader = () => (
-  <View style={[styles.container, styles.horizontal]}>
-    <ActivityIndicator size="large" color="#ccf0fa" />
-  </View>
-);
+const Loader = () => {
+  const animation = useRef(null);
+  return (
+    <View style={[styles.container]}>
+      <LottieView
+        autoPlay
+        loop
+        ref={animation}
+        style={{
+          width: 400,
+          height: 400,
+        }}
+        source={require("../assets/avatar1.json")}
+      />
+      <CustomHeadingText>Loading...</CustomHeadingText>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     backgroundColor: "#161622",
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
   },
-  horizontal: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    padding: 10,
-  },
+  // horizontal: {
+  //   flexDirection: "row",
+  //   justifyContent: "space-around",
+  //   padding: 10,
+  // },
 });
 
 export default Loader;
