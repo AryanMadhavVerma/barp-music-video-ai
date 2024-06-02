8078213ef7b180f211f8a1fc1ba0a62b42ac737f
Rajath Aithal <rajathlikeslemons@gmail.com>
2024-06-02 07:14:34 +0530

    updated code

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,8 @@ import json
 
 # from pyngrok import ngrok
 from get_cloned_voice import get_cloned_voice
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Request, Response, HTTPException
+from fastapi.responses import StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from gemini import get_prompt_for_suno
 from pydantic import Field
@@ -20,6 +21,7 @@ from utils.voice_utils.pitch_correction import pitch_correction
 from utils.voice_utils.generate_final_mix import generate_final_mix
 import ngrok
 import time
+from google.cloud import storage
 
 
 
@@ -73,27 +75,28 @@ async def predict(video_file: Optional[UploadFile], audio_file: Optional[UploadF
     voice_id = voice.voice_id
     print("voice id fetched")
     # call suno to get results
-    suno_song = generate_suno_song(rhyme=result['rhyme'], song_type=result['song_type'], title=result['title'])
-    with open('data.json', 'w') as f:
-        json.dump(suno_song, f)
-    print(suno_song)
-    url_to_download = suno_song[0].get("audio_url", "")
-    # url_to_download = "https://audiopipe.suno.ai/?item_id=ba548df5-2661-4e30-a7da-89af95b3fce1"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    # suno_song = generate_suno_song(rhyme=result['rhyme'], song_type=result['song_type'], title=result['title'])
+    # with open('data.json', 'w') as f:
+    #     json.dump(suno_song, f)
+    # print(suno_song)
+    # url_to_download = suno_song[0].get("audio_url", "")
+    # # url_to_download = "https://audiopipe.suno.ai/?item_id=ba548df5-2661-4e30-a7da-89af95b3fce1"
+    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-    tries, done = 0, False
-    while not done and tries <= 3:
-        print("Try", tries)
-        req = urllib.request.Request(url_to_download, headers=headers)
-        try:
-            with urllib.request.urlopen(req) as response, open(SUNO_SONG_FILE_PATH, 'wb') as out_file:
-                out_file.write(response.read())
-            done = True
-        except Exception as e:
-            print(f'HTTP Error: {e.code} {e.reason}')
-        tries += 1
-        time.sleep(20)
-    # remove voice from suno results
+    # tries, done = 0, False
+    # while not done and tries <= 3:
+    #     print("Try", tries)
+    #     req = urllib.request.Request(url_to_download, headers=headers)
+    #     try:
+    #         with urllib.request.urlopen(req) as response, open(SUNO_SONG_FILE_PATH, 'wb') as out_file:
+    #             out_file.write(response.read())
+    #         done = True
+    #     except Exception as e:
+    #         # print(f'HTTP Error: {e.code} {e.reason}')
+    #         print("Exception when downloading file")
+    #     tries += 1
+    #     time.sleep(20)
+    # # remove voice from suno results
 
     ##assuming we get an mp3 back
     
@@ -112,9 +115,65 @@ async def predict(video_file: Optional[UploadFile], audio_file: Optional[UploadF
     print("final_mix path is", final_mix)
     final_video = combine_video_audio(video_file_path="./data/temp/video_file.mp4", audio_file_path=final_mix)
 
+    upload_results_to_gcs()
     return {
         "message": "Success",
         "final_video": "final_video.mp4"
     }
 
 app.mount("/static", StaticFiles(directory="data/temp"), name="static")
+
+def upload_results_to_gcs():
+    storage_client = storage.Client()
+    bucket = storage_client.bucket("mayhem-hackathon")
+    blob = bucket.blob("final_video.mp4")
+    blob.upload_from_filename("./data/temp/final_video.mp4")
+    blob.make_public()
+
+
+# Helper function to handle byte-range requests
+def range_requests(request, file_path):
+    file_size = os.path.getsize(file_path)
+    range_header = request.headers.get("range")
+    if range_header:
+        range_start, range_end = range_header.replace("bytes=", "").split("-")
+        range_start = int(range_start)
+        range_end = int(range_end) if range_end else file_size - 1
+        content_length = (range_end - range_start) + 1
+        with open(file_path, "rb") as file:
+            file.seek(range_start)
+            data = file.read(content_length)
+            headers = {
+                "Content-Range": f"bytes {range_start}-{range_end}/{file_size}",
+                "Accept-Ranges": "bytes",
+                "Content-Length": str(content_length),
+                "Content-Type": "video/mp4",
+            }
+            return Response(data, status_code=206, headers=headers)
+    else:
+        with open(file_path, "rb") as file:
+            data = file.read()
+            headers = {
+                "Content-Length": str(file_size),
+                "Content-Type": "video/mp4",
+                "Accept-Ranges": "bytes",
+                "Content-Type": "video/mp4",
+            }
+            return Response(data, status_code=206, headers=headers)
+
+@app.get("/static/{video_name}")
+async def get_video(request: Request, video_name: str):
+    print('here ...')
+    file_path = os.path.join('data/temp', video_name)
+    if not os.path.isfile(file_path):
+        raise HTTPException(status_code=404, detail="Video not found")
+    return range_requests(request, file_path)
+
+
+@app.get("/video/play")
+async def video_endpoint():
+    def iterfile():
+        with open("data/temp/final_video.mp4", mode="rb") as file_like:
+            yield from file_like
+
+    return StreamingResponse(iterfile(), media_type="video/mp4")
